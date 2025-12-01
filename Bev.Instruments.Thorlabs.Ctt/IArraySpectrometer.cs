e4f3563eac6cca81feb2a1a15caa9fb5db773778
Michael Matus <michael@matus.at>
2025-12-01 10:30:41 +0100

    Use external abstraction

---
+++ Bev.Instruments.Thorlabs.Ctt/IArraySpectrometer.cs
@@ -0,0 +1,18 @@
+﻿namespace Bev.Instruments.Thorlabs.Ctt
+{
+    public interface IArraySpectrometer
+    {
+        string InstrumentManufacturer { get; }
+        string InstrumentType { get; }
+        string InstrumentSerialNumber { get; }
+        string InstrumentFirmwareVersion { get; }
+        
+        double[] Wavelengths { get; }        
+        double MinimumWavelength { get; }
+        double MaximumWavelength { get; }
+        
+        double[] GetIntensityData();
+        void SetIntegrationTime(double seconds);
+        double GetIntegrationTime();
+    }
+}
